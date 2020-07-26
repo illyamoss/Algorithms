8f031d8d2a432067c4adcdb948a6e7c9d72e027f
IllyaMoss <50335232+qXytreXp@users.noreply.github.com>
2020-07-26 21:20:21 +0300

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,14 +1,11 @@
 arr = [1,3,5,4,2]
 print(arr)
 
-
 def sort(arr):
-	count = len(arr)
-
-	for q in range(count-1):
-	    for j in range(count-q-1):
-	        if arr[j] > arr[j+1]:
-	            arr[j], arr[j+1] = arr[j+1], arr[j]
+    for q in range(len(arr)-1):
+        for j in range(len(arr)-q-1):
+	    if arr[j] > arr[j+1]:
+	        arr[j], arr[j+1] = arr[j+1], arr[j]
 	return arr
 
 print(sort(arr))
