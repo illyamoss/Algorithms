408855ff26d0df206654cb54277d693088c60b3e
IllyaMoss <50335232+qXytreXp@users.noreply.github.com>
2020-09-09 10:43:30 +0300

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -2,7 +2,7 @@ def factorial(item):
   res = 1
 
   for num in range(1, item+1):
-    search_factorail = res * num
-    res = search_factorail
+      search_factorail = res * num
+      res = search_factorail
 
   return res
